215def9c052b79056d1a7607b933e6c7563f7fce
MatthewZMD <matthewzmd@gmail.com>
2019-02-24 13:20:16 -0500

    Modified README

--- kilo.c
+++ kilo.c
@@ -1,15 +1,23 @@
+#include <stdlib.h>
 #include "termiWin.h" // termios port for Windows
 #include "unistd.h"
 
-void enableRawMode() {
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
+struct termios orig_termios;
 
+void disableRawMode(){
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+  
+  struct termios raw = orig_termios;
+  
   raw.c_lflag &= ~(ECHO);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
-
 
 int main(){
   enableRawMode();
