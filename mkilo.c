ea74657fb392a0bcd8bb0e24da21a8f714cc732a
Mingde Zeng <matthewzmd@gmail.com>
2019-02-28 23:55:39 -0500

    Define ctrl key

--- mkilo.c
+++ mkilo.c
@@ -6,6 +6,10 @@
 #include <unistd.h>
 #include <errno.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** data ***/
 
 struct termios orig_termios;
@@ -78,7 +82,9 @@ int main() {
     }else {
       printf("%d ('%c')\r\n", c , c);
     }
-    if (c == 'q') break;
+    if (c == CTRL_KEY('q')){
+      break;
+    }
   }
 
   return 0;
