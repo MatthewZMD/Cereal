1bf86e7c8d53800b2a4ff569e3f2c55319627013
Mingde Zeng <matthewzmd@gmail.com>
2019-02-28 23:33:23 -0500

    Raw mode made

--- mkilo.c
+++ mkilo.c
@@ -1,40 +1,85 @@
+/*** includes ***/
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <errno.h>
+
+/*** data ***/
 
 struct termios orig_termios;
 
+/*** terminal ***/
+
+void die(const char *s){
+  perror(s);
+  exit(1);
+}
+
 // restore terminal's original attributes on exit
 void disableRawMode(){
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1){
+    die("tcsetattr");
+  }
 }
 
 void enableRawMode(){
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if(tcgetattr(STDIN_FILENO, &orig_termios) == -1){
+    die("tcgetattr");
+  }
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  /* use bitwise-NOT operator (~) to disable */
-  /* ECHO -> print input onto the screen :: ECHO is a bitflag defined as 000..0001000 */
-  /* ICANON ->  canonical mode, reading input byte-by-byte instead of line-by-line*/
-  raw.c_lflag &= ~(ECHO | ICANON);
+  // use bitwise-NOT operator (~) to disable the following
+
+  // ECHO -> print input onto the screen :: ECHO is a bitflag defined as 000..0001000
+  // ICANON -> canonical mode, reading input byte-by-byte instead of line-by-line
+  // ISIG -> controls C-c SIGINT and C-z SIGSTP signal
+  // IEXTEN -> disable C-v
+
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+
+  // IXON -> software flow control C-s and C-q
+  // ICRNL -> Input Carriage Return New Line :: C-m
+  // BRKINT, INPICK, ISTRIP -> tradition to turn off in raw mode
+  raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+
+  // tradition
+  raw.c_iflag |= (CS8);
+
+  // OPOST -> Post processing of output :: \n to \r\n
+  raw.c_oflag &= ~(OPOST);
+
+  // timeout for read
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
 
   // TCSAFLUSH allows the leftover input no longer fed into the shell
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1){
+    die("tcsetattr");
+  }
 }
+
+
+/*** init ***/
 
 int main() {
   enableRawMode();
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+
+  while (1){
+    char c = '\0';
+    if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN){
+      die("read");
+    }
     // iscntrl tests whether a character is a control character
     if(iscntrl(c)){
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     }else {
-      printf("%d ('%c')\n", c , c);
+      printf("%d ('%c')\r\n", c , c);
     }
+    if (c == 'q') break;
   }
+
   return 0;
 }
