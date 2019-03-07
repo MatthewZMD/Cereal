076a6180e71c82de0c0e41f6f8b5d724f34e7bf2
Mingde Zeng <matthewzmd@gmail.com>
2019-03-06 21:34:17 -0500

    end of line cursor

--- mkilo.c
+++ mkilo.c
@@ -287,8 +287,6 @@ void editorScroll(){
   if(E.cx >= E.coloff + E.screencols){
     E.coloff = E.cx - E.screencols +1;
   }
-
-
 }
 
 // handles how drawing each row of the buffer of text being edited
@@ -366,6 +364,8 @@ void editorRefreshScreen(){
 /*** input ***/
 
 void editorMoveCursor(int key){
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key){
   case ARROW_UP:
     if(E.cy != 0){
@@ -380,11 +380,27 @@ void editorMoveCursor(int key){
   case ARROW_LEFT:
     if(E.cx != 0){
       --E.cx;
+    }else if(E.cy > 0){
+      // move cursor up
+      --E.cy;
+      E.cx = E.row[E.cy].size;
     }
     break;
   case ARROW_RIGHT:
-    ++E.cx;
+    if(row && E.cx < row->size){
+      ++E.cx;
+    }else if(row && E.cx == row->size){
+      ++E.cy;
+      E.cx = 0;
+    }
     break;
+  }
+
+  // cursor to the end of line when necessary
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if(E.cx > rowlen){
+    E.cx = rowlen;
   }
 }
 
