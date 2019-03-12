92686a008370ba4183d375b72b4a95c433b5fc37
Mingde Zeng <matthewzmd@gmail.com>
2019-03-12 08:31:19 -0400

    Render chars

--- mkilo.c
+++ mkilo.c
@@ -36,7 +36,9 @@ enum editorKey {
 
 typedef struct erow{
   int size;
+  int rsize;
   char * chars;
+  char * render;
 } erow;
 
 struct editorConfig {
@@ -212,6 +214,33 @@ int getWindowSize(int *rows, int *cols){
 
 /*** row operations ***/
 
+void editorUpdateRow(erow *row){
+  int tabs = 0;
+  int j;
+  for(j = 0; j < row->size; ++j){
+    if(row->chars[j] == '\t'){
+      ++tabs;
+    }
+  }
+  free(row->render);
+  row->render = malloc(row->size + tabs* 7  +1);
+
+
+  int idx = 0;
+  for(j = 0; j < row->size; ++j){
+    if(row->chars[j] == '\t'){
+      row->render[idx++] = ' ';
+      while(idx % 8 != 0){
+        row->render[idx++] = ' ';
+      }
+    } else {
+      row->render[++idx] = row->chars[j];
+    }
+  }
+  row->render[idx] = '\0';
+  row->rsize = idx;
+}
+
 void editorAppendRow(char *s, size_t len){
   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
 
@@ -220,6 +249,11 @@ void editorAppendRow(char *s, size_t len){
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
+
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
+  editorUpdateRow(&E.row[at]);
+
   ++E.numrows;
 }
 
@@ -234,6 +268,7 @@ void editorOpen(char *filename){
   char *line = NULL;
   size_t linecap = 0;
   ssize_t linelen = 13;
+
   linelen = getline(&line, &linecap, fp);
   while((linelen = getline(&line, &linecap, fp)) != -1){
     while(linelen > 0 && (line[linelen -1] == '\n' || line[linelen -1] == '\r')){
@@ -318,14 +353,14 @@ void editorDrawRows(struct abuf *ab){
         abAppend(ab, "~", 1);
       }
     }else{
-      int len = E.row[filerow].size - E.coloff;
+      int len = E.row[filerow].rsize - E.coloff;
       if(len < 0){
         len = 0;
       }
       if (len > E.screencols){
         len = E.screencols;
       }
-      abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+      abAppend(ab, &E.row[filerow].render[E.coloff], len);
     }
 
     // clear 1 line at a time
