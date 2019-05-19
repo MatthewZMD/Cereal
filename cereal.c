562dcf7e824775409c7e396c6f71c93425262423
Mingde Zeng <matthewzmd@gmail.com>
2019-05-18 22:40:00 -0400

    new line

--- cereal.c
+++ cereal.c
@@ -285,10 +285,12 @@ void editorUpdateRow(erow *row) {
     row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len) {
-    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+void editorInsertRow(int at, char *s, size_t len){
+    if (at < 0 || at > E.numrows) return;
 
-    int at = E.numrows;
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+    memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
     E.row[at].size = len;
     E.row[at].chars = malloc(len + 1);
     memcpy(E.row[at].chars, s, len);
@@ -336,6 +338,21 @@ void editorRowAppendString(erow *row, char *s, size_t len){
     ++E.dirty;
 }
 
+void editorInsertNewline() {
+    if (E.cx == 0){
+        editorInsertRow(E.cy, "", 0);
+    } else {
+        erow *row = &E.row[E.cy];
+        editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+        row = &E.row[E.cy];
+        row->size = E.cx;
+        row->chars[row->size] = '\0';
+        editorUpdateRow(row);
+    }
+    ++E.cy;
+    E.cx = 0;
+}
+
 void editorRowDelChar(erow *row, int at){
     if(at < 0 || at >= row->size) return;
 
@@ -349,7 +366,7 @@ void editorRowDelChar(erow *row, int at){
 
 void editorInsertChar(int c) {
     if (E.cy == E.numrows) {
-        editorAppendRow("", 0);
+        editorInsertRow(E.numrows, "", 0);
     }
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     ++E.cx;
@@ -409,7 +426,7 @@ void editorOpen(char *filename) {
                (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) {
             --linelen;
         }
-        editorAppendRow(line, linelen);
+        editorInsertRow(E.numrows, line, linelen);
     }
     free(line);
     fclose(fp);
@@ -664,7 +681,7 @@ void editorProcessKeypress() {
     // Use EMACS bindings
     switch (c) {
     case '\r':
-        /* TODO */
+        editorInsertNewline();
         break;
 
     case CTRL_KEY('a'):
