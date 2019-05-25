ad506d8f6cdaed4b4d607f82a1c09edece5edbac
Mingde Zeng <matthewzmd@gmail.com>
2019-05-24 23:59:17 -0400

    Some syntax-highlighting

--- cereal.c
+++ cereal.c
@@ -40,6 +40,12 @@ enum editorKey {
     DEL_KEY
 };
 
+enum editorHighlight {
+    HL_NORMAL = 0,
+    HL_NUMBER,
+    HL_MATCH
+};
+
 /*** data ***/
 
 typedef struct erow {
@@ -47,6 +53,7 @@ typedef struct erow {
     int rsize;
     char *chars;
     char *render;
+    unsigned char *hl;
 } erow;
 
 struct editorConfig {
@@ -247,6 +254,27 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+/*** syntax highlighting ***/
+
+void editorUpdateSyntax (erow *row) {
+    row->hl = realloc(row->hl, row->rsize);
+    memset(row->hl, HL_NORMAL, row->rsize);
+    for (int i = 0; i < row->rsize; ++i) {
+        if (isdigit(row->render[i])) {
+            row->hl[i] = HL_NUMBER;
+        }
+    }
+}
+
+// ANSI Colors. See https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+int editorSyntaxToColor (int hl) {
+    switch (hl){
+    case HL_NUMBER: return 31;
+    case HL_MATCH: return 34;
+    default: return 37;
+    }
+}
+
 /*** row operations ***/
 
 // convert chars-x to render-x
@@ -302,6 +330,8 @@ void editorUpdateRow(erow *row) {
     }
     row->render[idx] = '\0';
     row->rsize = idx;
+
+    editorUpdateSyntax(row);
 }
 
 void editorInsertRow(int at, char *s, size_t len){
@@ -317,6 +347,7 @@ void editorInsertRow(int at, char *s, size_t len){
 
     E.row[at].rsize = 0;
     E.row[at].render = NULL;
+    E.row[at].hl = NULL;
     editorUpdateRow(&E.row[at]);
 
     ++E.numrows;
@@ -326,6 +357,7 @@ void editorInsertRow(int at, char *s, size_t len){
 void editorFreeRow(erow *row){
     free(row->render);
     free(row->chars);
+    free(row->hl);
 }
 
 void editorDelRow(int at){
@@ -490,6 +522,15 @@ void editorSearchCallback(char *query, int key){
    static int last_match = -1;
    static int direction = 1;
 
+   static int saved_hl_line;
+   static char *saved_hl = NULL;
+
+   if (saved_hl) {
+       memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+       free(saved_hl);
+       saved_hl = NULL;
+   }
+
     if (key == '\r' || key == '\x1b' || key == CTRL_KEY('g')){
         last_match = -1;
         direction = 1;
@@ -523,6 +564,11 @@ void editorSearchCallback(char *query, int key){
             E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
+
+            saved_hl_line = current;
+            saved_hl = malloc(row->rsize);
+            memcpy(saved_hl, row->hl, row->rsize);
+            memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
         }
     }
@@ -534,8 +580,7 @@ void editorSearch() {
     int orig_coloff = E.coloff;
     int orig_rowoff = E.rowoff;
 
-    char *query = editorPrompt("Search: %s (ESC or C-g to Cancel | C-s to Search Forward | C-r to Search Backward)",
-                               editorSearchCallback);
+    char *query = editorPrompt("Search: %s (ESC or C-g to Cancel | C-s to Search Forward | C-r to Search Backward)", editorSearchCallback);
 
     if (query){
         free(query);
@@ -627,13 +672,30 @@ void editorDrawRows(struct abuf *ab) {
             }
         } else {
             int len = E.row[filerow].rsize - E.coloff;
-            if (len < 0) {
-                len = 0;
+            if (len < 0) len = 0;
+            if (len > E.screencols) len = E.screencols;
+            char *c = &E.row[filerow].render[E.coloff];
+            unsigned char *hl = &E.row[filerow].hl[E.coloff];
+            int current_color = -1;
+            for (int j = 0; j < len; ++j){
+                if (hl[j] == HL_NORMAL) {
+                    if (current_color != -1){
+                        abAppend(ab, "\x1b[39m", 5);
+                        current_color = -1;
+                    }
+                    abAppend(ab, &c[j], 1);
+                } else {
+                    int color = editorSyntaxToColor(hl[j]);
+                    if (color != current_color) {
+                        current_color = color;
+                        char buf[16];
+                        int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", color);
+                        abAppend(ab, buf, clen);
+                    }
+                    abAppend(ab, &c[j], 1);
+                }
             }
-            if (len > E.screencols) {
-                len = E.screencols;
-            }
-            abAppend(ab, &E.row[filerow].render[E.coloff], len);
+            abAppend(ab, "\x1b[39m", 5);
         }
 
         // clear 1 line at a time
