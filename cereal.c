5302f7d304fda18c59b5340392610090b8f242b5
Mingde Zeng <matthewzmd@gmail.com>
2019-06-05 20:38:31 -0400

    multiline comment colour

--- cereal.c
+++ cereal.c
@@ -46,6 +46,7 @@ enum editorKey {
 enum editorHighlight {
     HL_NORMAL = 0,
     HL_COMMENT,
+    HL_MLCOMMENT,
     HL_KEYWORD1,
     HL_KEYWORD2,
     HL_STRING,
@@ -56,11 +57,13 @@ enum editorHighlight {
 /*** data ***/
 
 typedef struct erow {
+    int idx;
     int size;
     int rsize;
     char *chars;
     char *render;
     unsigned char *hl;
+    int hl_open_comment;
 } erow;
 
 struct editorSyntax {
@@ -68,6 +71,8 @@ struct editorSyntax {
     char **filematch;
     char **keywords;
     char *singleline_comment_start;
+    char *multiline_comment_start;
+    char *multiline_comment_end;
     int flags;
 };
 
@@ -105,7 +110,7 @@ struct editorSyntax HLDB[] = {
         "c",
         C_HL_extensions,
         C_HL_keywords,
-        "//",
+        "//", "/*", "*/",
         HL_HIGHLIGHT_NUMBER | HL_HIGHLIGHT_STRINGS
     },
 };
@@ -307,20 +312,47 @@ void editorUpdateSyntax (erow *row) {
     char **keywords = E.syntax->keywords;
 
     char *scs = E.syntax->singleline_comment_start;
+    char *mcs = E.syntax->multiline_comment_start;
+    char *mce = E.syntax->multiline_comment_end;
+
     int scs_len = scs ? strlen(scs) : 0;
+    int mcs_len = mcs ? strlen(mcs) : 0;
+    int mce_len = mce ? strlen(mce) : 0;
 
     int prev_sep = 1;
     int in_string = 0;
+    int in_comment = (row->idx > 0 && E.row[row->idx - 1].hl_open_comment);
 
     int i = 0;
     while (i < row->rsize) {
         char c = row->render[i];
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
 
-        if (scs_len && !in_string) {
+        if (scs_len && !in_string && !in_comment) {
             if (!strncmp(&row->render[i], scs, scs_len)) {
                 memset(&row->hl[i], HL_COMMENT, row->rsize - i);
                 break;
+            }
+        }
+
+        if (mcs_len && mce_len && !in_string) {
+            if(in_comment){
+                row->hl[i] = HL_MLCOMMENT;
+                if (!strncmp(&row->render[i], mce, mce_len)){
+                    memset(&row->hl[i], HL_MLCOMMENT, mce_len);
+                    i += mce_len;
+                    in_comment = 0;
+                    prev_sep = 1;
+                    continue;
+                } else {
+                    ++i;
+                    continue;
+                }
+            } else if (!strncmp(&row->render[i], mcs, mcs_len)){
+                memset(&row->hl[i], HL_MLCOMMENT, mcs_len);
+                i += mcs_len;
+                in_comment = 1;
+                continue;
             }
         }
 
@@ -366,16 +398,22 @@ void editorUpdateSyntax (erow *row) {
             }
         }
 
-
         prev_sep = is_separator(c);
         ++i;
+    }
+
+    int changed = (row->hl_open_comment != in_comment);
+    row->hl_open_comment = in_comment;
+    if (changed && row->idx + 1 < E.numrows) {
+        editorUpdateSyntax(&E.row[row->idx + 1]);
     }
 }
 
 // ANSI Colors. See https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
 int editorSyntaxToColor (int hl) {
     switch (hl){
-    case HL_COMMENT: return 36;
+    case HL_COMMENT:
+    case HL_MLCOMMENT: return 36;
     case HL_STRING: return 35;
     case HL_KEYWORD1: return 33;
     case HL_KEYWORD2: return 32;
@@ -476,6 +514,11 @@ void editorInsertRow(int at, char *s, size_t len){
 
     E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
     memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+    for (int j = at + 1; j < E.numrows; ++j) {
+        ++E.row[j].idx;
+    }
+
+    E.row[at].idx = at;
 
     E.row[at].size = len;
     E.row[at].chars = malloc(len + 1);
@@ -485,6 +528,7 @@ void editorInsertRow(int at, char *s, size_t len){
     E.row[at].rsize = 0;
     E.row[at].render = NULL;
     E.row[at].hl = NULL;
+    E.row[at].hl_open_comment = 0;
     editorUpdateRow(&E.row[at]);
 
     ++E.numrows;
@@ -501,6 +545,9 @@ void editorDelRow(int at){
     if(at < 0 || at >= E.numrows) return;
     editorFreeRow(&E.row[at]);
     memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+    for (int j = at; j < E.numrows - 1; ++j) {
+        --E.row[j].idx;
+    }
     --E.numrows;
     ++E.dirty;
 }
@@ -819,22 +866,32 @@ void editorDrawRows(struct abuf *ab) {
             unsigned char *hl = &E.row[filerow].hl[E.coloff];
             int current_color = -1;
             for (int j = 0; j < len; ++j){
-                if (hl[j] == HL_NORMAL) {
-                    if (current_color != -1){
-                        abAppend(ab, "\x1b[39m", 5);
-                        current_color = -1;
-                    }
-                    abAppend(ab, &c[j], 1);
-                } else {
-                    int color = editorSyntaxToColor(hl[j]);
-                    if (color != current_color) {
-                        current_color = color;
-                        char buf[16];
-                        int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", color);
-                        abAppend(ab, buf, clen);
-                    }
-                    abAppend(ab, &c[j], 1);
+              if (iscntrl(c[j])) {
+                char sym = (c[j] <= 26) ? '@' + c[j] : '?';
+                abAppend(ab, "\x1b[7m", 4);
+                abAppend(ab, &sym, 1);
+                abAppend(ab, "\x1b[m", 3);
+                if (current_color != -1) {
+                    char buf[16];
+                    int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", current_color);
+                    abAppend(ab, buf, clen);
                 }
+              } else if (hl[j] == HL_NORMAL) {
+                if (current_color != -1) {
+                  abAppend(ab, "\x1b[39m", 5);
+                  current_color = -1;
+                }
+                abAppend(ab, &c[j], 1);
+              } else {
+                int color = editorSyntaxToColor(hl[j]);
+                if (color != current_color) {
+                  current_color = color;
+                  char buf[16];
+                  int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", color);
+                  abAppend(ab, buf, clen);
+                }
+                abAppend(ab, &c[j], 1);
+              }
             }
             abAppend(ab, "\x1b[39m", 5);
         }
